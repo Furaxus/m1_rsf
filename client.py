aef5a215d4aa50b8da84be7fe60de4a6178780f2
Furaxus <jeremy.zaragoza@gmail.com>
2019-02-23 19:21:17 +0100

    add client / server files, configuration script and test exec file

---
+++ client.py
@@ -0,0 +1,54 @@
+import socket                   # Import socket module
+import time
+import subprocess
+import sys
+import os
+
+def main():
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)             # Create a socket object
+
+    host = ""                   # Ip address that the TCPServer  is there
+    port = -1                   # Reserve a port for your service every new transfer wants a new port or you must wait.
+    filename = ""
+
+    config_file = open("config.conf")
+    for line in config_file:
+        param = line.split(' ')
+        if param[0] == "IP":
+            host = param[1]
+        elif param[0] == "PORT":
+            port = int(param[1])
+        elif param[0] == "FILENAME_CLT":
+            filename = param[1]
+            
+    config_file.close()
+
+    for i in range(5):
+        s.connect((host, port))
+        print("Communication number "+str(i))
+        with open(filename, 'wb') as f:
+            print ('file opened')
+            while True:
+                print('receiving data...')
+                data = s.recv(1024)
+                if data.decode()=='END':
+                    print('Receive END')
+                    break
+                # write data to a file
+                f.write(data)
+        f.close()
+        print('Successfully get the file')
+        commande=s.recv(1024).decode()
+        print('Receive command : '+ commande)
+        print('Execution...')
+        os.system('chmod u+x ' + filename)
+        if commande == "EXEC":
+            os.system('./' + filename)
+        print('End of execution')
+        s.close()
+        print('Connection closed')
+
+
+
+if __name__ == "__main__":  
+    main()
---
+++ server.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# A large part of this code provide from https://gist.github.com/giefko/2fa22e01ff98e72a5be2
+
+import sys                      # Import sys module
+import socket                   # Import socket module
+import time
+
+def main():
+    s = socket.socket()             # Create a socket object
+    
+    host = ""                       # Get local machine name
+    port = -1                       # Port to listen
+    filename = ""                   # Filename to execute
+    
+    config_file = open("config.conf")
+    for line in config_file:
+        param = line.split(' ')
+        if param[0] == "IP":
+            host = param[1]
+        elif param[0] == "PORT":
+            port = int(param[1])
+        elif param[0] == "FILENAME_CLT":
+            filename = param[1]
+            
+    config_file.close()
+    
+    s.bind((host, port))            # Bind to the port
+    s.listen(5)                     # Now wait for client connection.
+
+    print ('Server listening....')
+
+
+    while True:
+        conn, addr = s.accept()     # Establish connection with client.
+        print ('Got connection from ' + str(addr))
+        #data = conn.recv(1024)
+        #print('Server received'+ repr(data))
+
+        f = open(filename,'rb')
+        l = f.read(1024)
+        print(l)
+        while (l):
+            conn.send(l)
+            print('Sent ',repr(l))
+            l = f.read(1024)
+        f.close()
+
+        print('Done sending')
+        time.sleep(3)
+        conn.send('END'.encode())
+        conn.send('EXEC'.encode())
+        conn.close()
+        
+    
+
+if __name__ == "__main__":  
+    main()
