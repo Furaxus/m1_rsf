f390fc504344f60f51c6373a6d311c0e3d84cee4
Reno7777 <manupate.mc@gmail.com>
2019-03-13 11:44:17 +0100

    update addr connect

--- bluetooth/client_bluetooth.py
+++ bluetooth/client_bluetooth.py
@@ -3,7 +3,7 @@ from bluetooth import *
 # Create the client socket
 client_socket=BluetoothSocket( RFCOMM )
 
-client_socket.connect(("00:12:D2:5A:BD:E4", 3))
+client_socket.connect(("7C:B0:C2:4F:CC:74", 3))
 
 client_socket.send("Hello World")
 
