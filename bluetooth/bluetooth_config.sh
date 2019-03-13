906c35161466d2ef3058d8f019d17dc82177ba58
Furaxus <jeremy.zaragoza@gmail.com>
2019-03-13 11:29:21 +0100

    update

--- bluetooth/bluetooth_config.sh
+++ bluetooth/bluetooth_config.sh
@@ -1,19 +1,24 @@
 #! /bin/sh
 
-#address of the device to connect
-address = "00:00:00:00:00:00"
-
-if [ $# -gt 2 ] then
-    echo "Usage : $0 <bluetooth_adress>"
+if [ $# -gt 3 || ($# -eq 3 && $1 != "client") ] then
+    echo "Usage : $0 [ client <bluetooth_adress_to connect> | server | config ]"
     exit 1
-elif [ $# -eq 1 ] then
+elif [ $# -eq 2 && $1 == "config"] then
     #print the bluetooth config of the device
     hciconfig
     exit 0
-elif [ $# -eq 2 ] then
-    #init the different variable
-    address = $1
 fi
 
-#pair with the device at the $address address
-pair $address
+
+bluetoothctl
+power on
+agent on
+if [ $1 == "server" ] then
+    #make device visible
+    discoverable on
+    scan on
+else
+    scan on
+    #pair with the device at the $address address
+    pair $2
+fi
